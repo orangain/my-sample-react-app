2722c0ce760ad719125ca3aeb93e2664ac26d336
orangain <orangain@gmail.com>
2020-01-25 13:07:27 +0900

    Rename files

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 
 import "./index.css";
-import App from "./App";
+import App from "./app";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
